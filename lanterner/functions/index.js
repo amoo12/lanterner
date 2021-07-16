dc644099cf5026b548010c1f55792f04bede1690
Abdulrahman al-abidi <apody3000@gmail.com>
2021-07-16 19:46:24 +0300

    folllow cloud function

---
+++ lanterner/functions/index.js
@@ -0,0 +1,50 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+/* eslint-disable */
+exports.onCreateFollower = functions.firestore
+  .document("/users/{uid}/followers/{followerId}")
+  .onCreate((snapshot, context) => {
+    console.log("Follower Created", snapshot.data());
+    const uid = context.params.uid;
+    const followerId = context.params.followerId;
+
+    //1) get followed user posts
+    const followedUserPostsRef = admin.firestore().collection("posts").where("user.uid", "==", uid);
+
+    // 2) create following user's timeline reference
+    const timelinePostsRef = admin
+      .firestore()
+      .collection("timeline")
+      .doc(followerId)
+      .collection("timelinePosts");
+
+    //   3) get followed user timeline posts
+    // const querySanpshot =
+    followedUserPostsRef.get().then((docs) => {
+      docs.forEach((doc) => {
+        if (doc.exists) {
+          const postId = doc.id;
+          const postData = doc.data();
+          timelinePostsRef.doc(postId).set(postData);
+        }
+      });
+    });
+
+    // 4) add each user post to following user snapshot
+
+    // querySanpshot.forEach((doc) => {
+    //   if (doc.exists) {
+    //     const postId = doc.id;
+    //     const postData = doc.data();
+    //     timelinePostsRef.doc(postId).set(postData);
+    //   }
+    // });
+  });
